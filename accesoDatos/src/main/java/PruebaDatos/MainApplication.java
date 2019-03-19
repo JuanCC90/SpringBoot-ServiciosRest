c109516cd9b3bd397640e6445e82570cc1217f4b
Admin <Admin@10.12.1.198>
2019-03-19 08:13:14 +0100

    Implementando JSF al proyecto

--- accesoDatos/src/main/java/PruebaDatos/MainApplication.java
+++ accesoDatos/src/main/java/PruebaDatos/MainApplication.java
@@ -1,20 +1,31 @@
 package PruebaDatos;
 
+import javax.faces.webapp.FacesServlet;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
 	
 	private static final Logger log= LoggerFactory.getLogger(MainApplication.class);
 	
 	public static void main(String []args) {
 		SpringApplication.run(MainApplication.class);
+	}
+	
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean() {
+		FacesServlet servlet = new FacesServlet();
+		ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(servlet, "*.jsf");
+		return servletRegistrationBean;
 	}
 	
 }
